e02d3208ab3fc632ba3835222ad6afd19a70d36e
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-06-13 00:00:56 +0600

    Update

--- src/db/init_db.py
+++ src/db/init_db.py
@@ -1,7 +1,7 @@
 import databases
 import sqlalchemy as sa
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
+DATABASE_URL = "postgresql://t2elzeth:postgres@localhost/db"
 
 metadata = sa.MetaData()
 
